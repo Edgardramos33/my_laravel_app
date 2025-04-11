78be5309fd21320ca8fa0a0270f4470804f92970
Edgardramos33 <edgardramoz15@gmail.com>
2025-04-11 08:21:12 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <style>
+        body {
+            display:flex;
+            justify-content: center;
+            align-items:center;
+            flex-direction:column;
+        }
+        .login-form {
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+        }
+        .user-login {
+            margin-bottom:10px;
+            height:20px;
+        }
+        .login-btn {
+            height:30px;
+            border-radius: 2px;
+            margin-bottom: 10px;
+        }
+        .login-btn a {
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <h2>Login</h2>
@@ -13,11 +38,12 @@
     @if($errors->any())
         <p style="color: red;">{{ $errors->first() }}</p>
     @endif
-    <form method="POST" action="{{ route('login') }}">
+    <form class="login-form" method="POST" action="{{ route('login') }}">
         @csrf
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
+        <input class="user-login" type="email" name="email" placeholder="Email" required>
+        <input class="user-login" type="password" name="password" placeholder="Password" required>
+        <button class="login-btn" type="submit">Login</button>
+        <button class="login-btn"><a href="{{ route('register') }}">Register</a></button>
     </form>
 </body>
 </html>
